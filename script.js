93456e6b3987c9dbef9aaebcb2c5d982e4c4b658
askjoshallen <44271523+rcd13@users.noreply.github.com>
2024-12-11 19:12:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
         { text: "Go Bills", image: "images/potato" },
         { text: "Stay humble", image: "images/josh1" },
         { text: "We got a great team this year and I have a good feeling about this", image: "images/josh2" },
-        { text: "We're just taking it one game at a time", image: "images/dont-count-on-it.png" },
-        { text: "You got to continue to trust the process, stick to what you know, learn from what happened and move on", image: "images/outlook-bad.png" },
-        { text: "I'm just trying to be the best quarterback possible.", image: "images/yes-definitely.png" },
-        { text: "At the end of the day, it's not where you get drafted. It's being surrounded by the right people, the right players, the right coaches, how you present yourself, how you work hard.", image: "images/rely-on-it.png" },
-        { text: "Very doubtful", image: "images/doubtful.png" },
+        { text: "We're just taking it one game at a time", image: "images/potato" },
+        { text: "You got to continue to trust the process, stick to what you know, learn from what happened and move on", image: "images/potato" },
+        { text: "I'm just trying to be the best quarterback possible.", image: "images/potato" },
+        { text: "At the end of the day, it's not where you get drafted. It's being surrounded by the right people, the right players, the right coaches, how you present yourself, how you work hard.", image: "images/potato" },
+        { text: "Very doubtful", image: "images/potato" },
     ];
 
     const shakeButton = document.getElementById('shake-button');
