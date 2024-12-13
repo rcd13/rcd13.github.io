eabcb708140e57faa3c3222a3a1cdee531408d54
askjoshallen <44271523+rcd13@users.noreply.github.com>
2024-12-13 14:22:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ document.addEventListener('DOMContentLoaded', function () {
         { text: "We got a great team this year and I have a good feeling about this", image: "images/josh4" },
         { text: "We're just taking it one game at a time", image: "images/josh5" },
         { text: "You got to continue to trust the process, stick to what you know, learn from what happened and move on", image: "images/josh6" },
-        { text: "I'm just trying to be the best quarterback possible.", image: "images/potato" },
-        { text: "At the end of the day, it's not where you get drafted. It's being surrounded by the right people, the right players, the right coaches, how you present yourself, how you work hard.", image: "images/potato" },
-        { text: "You can't focus on the noise. It's about staying grounded and taking things one play at a time.", image: "images/potato" },
-        { text: "Adversity is like the wind off Lake Erie—cold and unforgiving, but it makes you tougher if you face it head-on.", image: "images/potato" },
-        { text: "No one circles the wagons like the Buffalo Bills. That’s the mentality you need: stick together, trust your team, and fight for each other.", image: "images/potato" },
+        { text: "I'm just trying to be the best quarterback possible.", image: "images/josh7" },
+        { text: "At the end of the day, it's not where you get drafted. It's being surrounded by the right people, the right players, the right coaches, how you present yourself, how you work hard.", image: "images/josh8" },
+        { text: "You can't focus on the noise. It's about staying grounded and taking things one play at a time.", image: "images/josh9" },
+        { text: "Adversity is like the wind off Lake Erie—cold and unforgiving, but it makes you tougher if you face it head-on.", image: "images/josh10" },
+        { text: "No one circles the wagons like the Buffalo Bills. That’s the mentality you need: stick together, trust your team, and fight for each other.", image: "images/josh11" },
     ];
 
     const shakeButton = document.getElementById('shake-button');
